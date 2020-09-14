eaa44a20c7d273807aae302564cf2d5090d81480
Strong9371 <1044256374@qq.com>
2020-09-14 23:56:29 +0800

    Signed-off-by: Strong9371 <1044256374@qq.com>

---
+++ src/main/java/com/wuhanyunzhong/itass/ItassApplication.java
@@ -0,0 +1,13 @@
+package com.wuhanyunzhong.itass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItassApplication.class, args);
+    }
+
+}
