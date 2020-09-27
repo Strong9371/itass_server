93c53fcdf6be8bb4a9775ff8b354adb377190db3
Strong9371 <1044256374@qq.com>
2020-09-27 23:52:42 +0800

    Signed-off-by: Strong9371 <1044256374@qq.com>

---
+++ src/test/java/com/wuhanyunzhong/itass/DemoData.java
@@ -0,0 +1,22 @@
+package com.wuhanyunzhong.itass;
+import lombok.Data;
+
+@Data
+public class DemoDate {
+    private String A;
+    private String B;
+    private String C;
+    private String D;
+    private String E;
+    private String F;
+    private String G;
+    private String H;
+    private String I;
+    private String J;
+    private String K;
+    private String L;
+    private String M;
+    private String N;
+    private String O;
+
+}
